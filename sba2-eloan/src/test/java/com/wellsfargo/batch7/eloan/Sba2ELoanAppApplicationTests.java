62defb36a76c5610e7636e9406d5d3cb3ab76b7a
Soumyarup <70505398+SoumyarupGhosh@users.noreply.github.com>
2020-12-07 19:07:54 +0530

    Final code sba 2 eloan application.
    Final code sba 2 eloan application.

---
+++ sba2-eloan/src/test/java/com/wellsfargo/batch7/eloan/Sba2ELoanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wellsfargo.batch7.eloan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sba2ELoanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
